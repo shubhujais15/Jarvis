284a39b48b46add8be8675b55717d0098804e966
shubhujais15 <shubhujaisedu15@gmail.com>
2024-12-22 17:55:49 +0530

    music list

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "emptiness" : "https://youtu.be/yUu26tcUri0?si=TTmeLiZneJpjbULT",
+    "baby" : "https://youtu.be/kffacxfA7G4?si=OWndByRUCohsvqZR",
+    "sorry" : "https://youtu.be/fRh_vgS2dFE?si=34IPSodgz4FKvzO_"
+}
